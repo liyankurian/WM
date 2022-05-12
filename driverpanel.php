23d9bdabfce8983615c2256c330c2ffc1941b9be
Liyan Susan Kurian <liyansusankurian@mca.ajce.in>
2022-05-12 12:41:41 +0530

    ss

--- driverpanel.php
+++ driverpanel.php
@@ -14,6 +14,7 @@ $spic=$userData['img'];
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
     <meta charset="UTF-8">
     <title> User Panel </title>
@@ -22,28 +23,47 @@ $spic=$userData['img'];
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- icons -->
-    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet"
+        href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <!--Bootstrap-->
     <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
     <!--Bootstrap js-->
     <script src="/css/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<style>
-    .row1 {
-        margin-left: 30px;
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <style>
+
+
+    .blinking {
+        animation: blinkingText 1.2s infinite;
     }
-    .blinking{
-    animation:blinkingText 1.2s infinite;
-}
-@keyframes blinkingText{
-    0%{     color: #fff;    }
-    49%{    color: #fff; }
-    60%{    color: transparent; }
-    99%{    color:transparent;  }
-    100%{   color: #fff;    }
-}
-</style>
+
+    @keyframes blinkingText {
+        0% {
+            color: #fff;
+        }
+
+        49% {
+            color: #fff;
+        }
+
+        60% {
+            color: transparent;
+        }
+
+        99% {
+            color: transparent;
+        }
+
+        100% {
+            color: #fff;
+        }
+    }
+    </style>
 </head>
 
 <body>
@@ -61,15 +81,15 @@ $spic=$userData['img'];
                     <li><a class="link_name" href="#">Dashboard</a></li>
                 </ul>
             </li>
-             <li>
+            <li>
                 <a href="dassigneduser.php" class="active">
-                    <i class='bx bx-user-pin' style='color:#ffffff'  ></i>
+                    <i class='bx bx-user-pin' style='color:#ffffff'></i>
                     <span class="link_name">Assigned Users</span>
                 </a>
                 <ul class="sub-menu blank">
                     <li><a class="link_name" href="dassigneduser.php">Assigned Users</a></li>
                 </ul>
-            </li> 
+            </li>
 
             <li>
                 <div class="profile-details">
@@ -80,7 +100,7 @@ $spic=$userData['img'];
                         <div class="profile_name"><a href="logout.php" style="color:white;"><i
                                     class='bx bx-log-out'></i>Driver</a></div>
                     </div>
-                    <img src="./staff pic/<?php echo $spic; ?>" height="100px" width="100px"/>
+                    <img src="./staff pic/<?php echo $spic; ?>" height="100px" width="100px" />
                 </div>
             </li>
         </ul>
@@ -93,85 +113,91 @@ $spic=$userData['img'];
             </div>
             <h4><?php echo $ah; ?></h4>
         </nav>
-        <div class="row1  ">
-        <?php
+        <div class="row1 ">
+            <?php
             date_default_timezone_set('Asia/Kolkata');
             $curdate=date("Y-m-d");
-            $sql = "SELECT * FROM tbl_pickupdetails WHERE  pickupdate='$curdate' AND  assign='$ah' AND status='1'";
-            $result = mysqli_query($con, $sql);
-            $resultCheck = mysqli_num_rows($result);
+            $sql = mysqli_query($con,"SELECT * FROM tbl_pickupdetails WHERE  pickupdate='$curdate' AND  assign='$ah' AND status='1'");
+            $resultCheck = mysqli_num_rows($sql);
+            $s = mysqli_query($con,"SELECT * FROM tbl_pickupdetails WHERE  pickupdate<'$curdate' AND  assign='$ah' AND status='2'");
+            $resultCheck1=mysqli_num_rows($s);
             ?>
-        <div class="form bg-light   m-4" id="form" style="height:auto; width:auto;">
-                <table class="table table-bordered">
-                   <tr>
-                    <th colspan="11" style="text-align:center; ">Today PickUp</th>
+            <div class="form bg-light m-4" id="form" style="height:auto; width:auto;">
+                <table class="table table-bordered " >
+                    <tr>
+                        <th colspan="11" style="text-align:center; ">Today PickUp</th>
                     </tr>
                     <tr>
-                    <th>Apartment Name</th>
-                    <th>Apartment Address</th>
-                    <th>District</th>
-                    <th>City</th>
-                    <th>Pincode</th>
-                    <th>Phone No</th>
-                    <th>Alternative No</th>
-                    <th scope="col">More</th>
-                    <TH>Collection Status</TH>
+                        <th>Apartment Name</th>
+                        <th>Apartment Address</th>
+                        <th>District</th>
+                        <th>City</th>
+                        <th>Pincode</th>
+                        <th>Phone No</th>
+                        <th>Alternative No</th>
+                        <th scope="col">More</th>
+                        <TH colspan="2">Collection Status</TH>
                     </tr>
                     <?php
                 
-                    while ($row = mysqli_fetch_assoc($result)) {
+                
+                
+              
+
+                    
+                    if ($resultCheck > 0 || $resultCheck1 > 0 ) {
+                        
+                    while ($row = mysqli_fetch_array($s)){ 
                     $aid = $row['uid'];
-                    if ($resultCheck > 0) {
                     $qr1=mysqli_query($con,"SELECT * FROM `tbl_userdetails` WHERE `uid`=$aid");
                     if($r=mysqli_fetch_array($qr1)){
                         ?>
-                        <tr>
-                            <td><?php echo $r['apname'];?></td>
-                            <td><?php echo $r['apno'];?></td>
-                            <td><?php echo $r['address'];?></td>
-                            <td><?php echo $r['dist'];?></td>
-                            <td><?php echo $r['city'];?></td>
-                            <td><?php echo $r['pin'];?></td>
-                            <td><?php echo $r['mob'];?></td>
-                            <td><a href="duserdetails.php?aab=<?php echo $r["uid"]; ?>"> <input class="bg-primary text-white"  type="submit" value="More Details" ></a></td>
-                            <td><button class="primary">Collected</button>
-                            </td>
-                            <td>
-                            <span class=" blinking badge rounded-pill bg-danger ">New User</span>
-                            </td>
-                            <td></td>
-                            
-                        </tr>
+                    <tr>
+                        <td><?php echo $r['apname'];?><br><span class=" blinking badge rounded-pill bg-danger ">New User</span></td>
                         
-                        <?php  
-                } 
-                }}
-
-                $s = mysqli_query($con,"SELECT * FROM tbl_pickupdetails WHERE  pickupdate<'$curdate' AND  assign='$ah' AND status='2'");
-                if(mysqli_num_rows($s)>0)
-                while ($row = mysqli_fetch_assoc($result)) {
+                        <td><?php echo $r['address'];?></td>
+                        <td><?php echo $r['dist'];?></td>
+                        <td><?php echo $r['city'];?></td>
+                        <td><?php echo $r['pin'];?></td>
+                        <td><?php echo $r['mob'];?></td>
+                        <td><?php echo $r['altmob'];?></td>
+                        <td><a href="duserdetails.php?aab=<?php echo $r["uid"]; ?>"> <input
+                                    class="bg-primary text-white" type="submit" value="More Details"></a></td>
+                        <td><button class="primary">Collected</button></td>
+                        
+                    </tr>
+                    <?php  
+                    }
+                }
+                while ($row = mysqli_fetch_array($sql)){
                     $aid = $row['uid'];
-                    $qr2=mysqli_query($con,"SELECT * FROM `tbl_userdetails` WHERE `uid`=$aid");
-                    if($re=mysqli_fetch_array($qr2)){
-                {
-                
+                    $qr1=mysqli_query($con,"SELECT * FROM `tbl_userdetails` WHERE `uid`=$aid");
+                    if($r=mysqli_fetch_array($qr1)){
                         ?>
-                        <tr>
-                            <td><?php echo $re['apname'];?></td>
-                            <td><?php echo $re['apno'];?></td>
-                            <td><?php echo $re['address'];?></td>
-                            <td><?php echo $re['dist'];?></td>
-                            <td><?php echo $re['city'];?></td>
-                            <td><?php echo $re['pin'];?></td>
-                            <td><?php echo $re['mob'];?></td>
-                            <td><a href="duserdetails.php?aab=<?php echo $re["uid"]; ?>"> <input class="bg-primary text-white"  type="submit" value="More Details" ></a></td>
-                            <td><button class="btn primary">Collect</button></td>
-                        </tr>
+                    <tr>
+                        <td><?php echo $r['apname'];?></td>
+                        <td><?php echo $r['address'];?></td>
+                        <td><?php echo $r['dist'];?></td>
+                        <td><?php echo $r['city'];?></td>
+                        <td><?php echo $r['pin'];?></td>
+                        <td><?php echo $r['mob'];?></td>
+                        <td><?php echo $r['altmob'];?></td>
+                        <td><a href="duserdetails.php?aab=<?php echo $r["uid"]; ?>"> <input
+                                    class="bg-primary text-white" type="submit" value="More Details"></a></td>
+                        <td><button class="primary">Collected</button>
+                        </td>
                         
-                        <?php }}}?>
-        
-        </table>
-        </div>
+                    </tr>
+                    <?php  
+                    }
+                }
+                
+                }else{?>
+                    <td>No Records</td>
+               <?php }?>
+
+                </table>
+            </div>
     </section>
     <script>
     let arrow = document.querySelectorAll(".arrow");
